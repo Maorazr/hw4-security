7c5b201cda74dba61572abe437c12633e28b18bc
Maorazr <maoraz@post.bgu.ac.il>
2023-05-29 18:15:16 +0300

    added Spinner loading icon

--- pages/create.tsx
+++ pages/create.tsx
@@ -3,18 +3,21 @@ import Layout from "../components/Layout";
 import Router from "next/router";
 import { useSession } from "next-auth/react";
 import submitPost from "./submitPost";
+import Spinner from "../components/Spinner";
 
 const Draft: React.FC = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [file, setFile] = useState<File | null>(null);
   const { data: session, status } = useSession();
+  const [isLoading, setIsLoading] = useState(false);
   let email = session?.user?.email;
 
   const submitData = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    setIsLoading(true);
     await submitPost(title, content, session, email, file);
+    setIsLoading(false);
   };
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -23,30 +26,33 @@ const Draft: React.FC = () => {
   };
   return (
     <Layout>
-      <div>
-        <form onSubmit={submitData}>
-          <h1>New Draft</h1>
-          <input
-            autoFocus
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title"
-            type="text"
-            value={title}
-          />
-          <textarea
-            cols={50}
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="Content"
-            rows={8}
-            value={content}
-          />
-          <input type="file" accept="video/*" onChange={handleFileChange} />
-          <input disabled={!content || !title} type="submit" value="Create" />
-          <a className="back" href="#" onClick={() => Router.push("/")}>
-            or Cancel
-          </a>
-        </form>
-      </div>
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <div>
+          <form onSubmit={submitData}>
+            <h1>New Draft</h1>
+            <input
+              autoFocus
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              type="text"
+              value={title}
+            />
+            <textarea
+              cols={50}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Content"
+              rows={8}
+              value={content}
+            />
+            <input type="file" accept="video/*" onChange={handleFileChange} />
+            <input disabled={!content || !title} type="submit" value="Create" />
+            <a className="back" href="#" onClick={() => Router.push("/")}>
+              or Cancel
+            </a>
+          </form>
+        </div>
+      )}
       <style jsx>{`
         .page {
           background: white;
